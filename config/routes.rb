cde39076163bea47f74dff746d14359747e42fb0
Mark Edward Ratio <markedwardratio@gmail.com>
2020-05-11 09:54:01 +0800

    Add flight picking

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root      'flights#index'
   resources :flights,   only: [:index]
   post      '/flights', to: 'flights#index'   
+  resources :bookings,  only: [:new]
 end
